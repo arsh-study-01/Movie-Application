9b5789fd9228fee75d4312a83b9584e2d1746735
Arshnoor <study.arshnoorkirmani@gmail.com>
2025-02-14 12:43:33 +0530

    Add Mobile Hinden Header

--- src/js/indexscript.js
+++ src/js/indexscript.js
@@ -8,6 +8,17 @@ function hide_Show(cls) {
     console.log("arshnoor");
   }
 }
+document.querySelector(".hiden-header").addEventListener("click", (elm) => {
+  addEventListener("click", (e) => {
+    if (e.target.offsetParent == elm.currentTarget) {
+      if (elm.target.offsetParent) {
+        elm.target.offsetParent.classList.toggle("hide");
+      }
+    }
+  });
+});
+// ==============================================================================
+
 let Header_options = document.querySelectorAll(".option-box > ul >li");
 Header_options.forEach((elm) => {
   elm.addEventListener("click", (e) => {
@@ -18,8 +29,17 @@ Header_options.forEach((elm) => {
     document.querySelector(".option-box").classList.add("hide");
   });
 });
+// ================================================================================
 let mobile_Header_btn = document.querySelector("header .search-input button");
 mobile_Header_btn.onclick = () => {
   let searchBox = document.querySelector(".search-input");
-  searchBox.classList.toggle("mobile-header-search-box")
+  searchBox.classList.toggle("mobile-header-search-box");
 };
+
+// ================================================================================
+let mobile_menu_options = document.querySelector(".links");
+mobile_menu_options.addEventListener("click", (e) => {
+  if (e.target.nodeName == "H1") {
+    e.target.parentElement.classList.toggle("mobile-hiden-header-options");
+  }
+});
